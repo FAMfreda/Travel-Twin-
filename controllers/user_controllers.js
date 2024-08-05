a5af52ffa0eb13aecb17e72fda91d9bcb5c7f0cb
FAMfreda <mbordfreda@gmail.com>
2024-08-05 13:30:39 +0000

    worked on users

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -1,9 +1,11 @@
 import { userModel } from "../model/user_models.js";
+import bcrypt from "bcrypt"
+import JWT from "jsonwebtoken"
 
 //post route
-export const userRegistration = async (req, res, next) => {
+export const register = async (req, res, next) => {
   try {
-    const hashPassword =brypt.hashsync(req.body.password, 10);
+    const hashPassword =bcrypt.hashSync(req.body.password, 10);
      const addData = await userModel.create({...req.body, 
       password:hashPassword
   });
@@ -18,21 +20,42 @@ export const userRegistration = async (req, res, next) => {
   }
 
   //get user  for now getting login details
-  const userLogin = async (req,res, next) =>{
+  export const userLogin = async (req,res, next) =>{
   try {
-      const getData = await userModel.find(req.body)
-      res.status(201).json(getData)
+      const getData = await userModel.find({
+        $or: [
+        {userName: req.body.userName},
+        {email: req.body.email}
+    ]
+    });
+
+    if (!user )
+      return res.status(404).json('user not found')
   
   } catch (error) {
     next(error)
     
+
+    const correctPassword = bcrypt.hashSync(req.body.password,user.password)
+    if (!correctPassword)
+
+        return res.status(404).json('incorrect username or password')
+
+        const token = JWT.sign(
+            {Id:user.Id},
+            process.env.JWT_PRIVATE_KEY,
+        {expiresIn: '24h'});
+
+        res.status(200).json({message:'user logged in successfully', accessToken : token})
+
+
   }
   }
 
 
 
   //user log out
-  const logoutUser = async(req,res,next) => {
+  export const logoutUser = async(req,res,next) => {
     res.status(201).json ('user logged out successfully ')
   }
   //get One User
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { dbConnection } from "./config/db.js";
+import mongoose from "mongoose";
+import userRouter from "./router/user_router.js";
 
 
 //create server
@@ -8,6 +10,12 @@ const app = express()
 
 //create Database
 dbConnection()
+
+// use middleware
+app.use(express.json ())
+
+//use router
+app.use(userRouter)
 
 
 //create port
